f62db49aa9de211ad5e7b6f5052e97927160aea1
shaoshao <1600767556@qq.com>
2022-04-04 20:17:06 +0800

    Stream 实体类

---
+++ src/StreamTest/Employee.java
@@ -0,0 +1,102 @@
+package streamtest;
+
+import java.util.Objects;
+
+/**
+ * @author shaoshao
+ * @version 1.0
+ * @date 2022/4/4 13:28
+ */
+
+public class Employee {
+    private Integer id;
+    private String name;
+    private Integer age;
+    private Double salary;
+    private Status status;
+
+    public Employee() {
+    }
+
+    public Employee(Integer id, String name, Integer age, Double salary, Status status) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+        this.status = status;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                ", status=" + status +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(id, employee.id) &&
+                Objects.equals(name, employee.name) &&
+                Objects.equals(age, employee.age) &&
+                Objects.equals(salary, employee.salary) &&
+                status == employee.status;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age, salary, status);
+    }
+
+    public enum Status{
+        FREE,
+        BUSY,
+        VOCATION;
+    }
+}
---
+++ src/streamtest/Employee.java
@@ -0,0 +1,9 @@
+package StreamTest;
+
+/**
+ * @author shaoshao
+ * @version 1.0
+ * @date 2022/4/4 13:28
+ */
+public class Employee {
+}
